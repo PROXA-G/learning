771601b7d32df8987f1a26170b10ac23adb0c4ba
PROXA-G <grandmasteroogway556@gmail.com>
2024-09-20 00:52:28 +0500

    How to Enqueue Scripts / Styles

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -41,12 +41,15 @@ class MyPlugin
         add_action('init', [ $this, 'custom_post_type' ]);
     }
 
+    public function register()
+    {
+        add_action('admin_enqueue_scripts', [ $this, 'enqueue' ]);
+    }
+
     public function activate()
     {
-
         $this->custom_post_type();
         flush_rewrite_rules();
-
     }
 
     public function deactivate()
@@ -63,10 +66,17 @@ class MyPlugin
     {
         register_post_type('book', [ 'public' => true, 'label' => 'Books' ]);
     }
+
+    public function enqueue()
+    {
+        wp_enqueue_style('myStyles', plugins_url('/assets/css/myStyles.css', __FILE__));
+        wp_enqueue_script('myScript', plugins_url('/assets/js/myScript.js', __FILE__));
+    }
 }
 
 if (class_exists('MyPlugin')) {
     $myPlugin = new MyPlugin();
+    $myPlugin->register();
 }
 
 register_activation_hook(__FILE__, [ $myPlugin, 'activate' ]);
