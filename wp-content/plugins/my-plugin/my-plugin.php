581a13d0062d5549de3d2ecbcd78427c94cced19
PROXA-G <grandmasteroogway556@gmail.com>
2024-09-20 16:44:42 +0500

    How to add Admin page & Plugin Setting link

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -32,52 +32,78 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 if (!defined('ABSPATH')) {
     die;
 }
-
-class MyPlugin
-{
-
-    public function __construct()
+if (!class_exists('MyPlugin')) {
+    class MyPlugin
     {
-        add_action('init', [ $this, 'custom_post_type' ]);
+        public $name;
+
+        public function __construct()
+        {
+            $this->name = plugin_basename(__FILE__);
+        }
+
+        public function register()
+        {
+            add_action('admin_enqueue_scripts', [ $this, 'enqueue' ]);
+
+            // This is a action for adding the Page in the Admin_Menu
+            add_action('admin_menu', [ $this, 'add_admin_pages' ]);
+
+            add_filter("plugin_action_links_$this->name", [ $this, 'settings_link' ]);
+        }
+
+        public function settings_link($links)
+        {
+            $settings_link = "<a href='admin.php?page=my_plugin_page'>Settings</a>";
+            array_push($links, $settings_link);
+            return $links;
+        }
+
+        public function add_admin_pages()
+        {
+            // We are adding the pages using this "add_menu_page" section
+            add_menu_page('My Plugin Page', 'MyPlugin', 'manage_options', 'my_plugin_page', [ $this, 'admin_page_template' ], 'dashicons-store', 110);
+        }
+
+        public function admin_page_template()
+        {
+            require_once plugin_dir_path(__FILE__) . 'templates/admin.php';
+            // Here we will require the PAGE from Templates
+        }
+
+        protected function create_post_type()
+        {
+            add_action('init', [ $this, 'custom_post_type' ]);
+        }
+
+        public function activate()
+        {
+            require_once plugin_dir_path(__FILE__) . 'inc/my-plugin-activate.php';
+            MyPluginActivate::activate();
+        }
+
+        public function deactivate()
+        {
+            require_once plugin_dir_path(__FILE__) . 'inc/my-plugin-deactivate.php';
+            MyPluginDeactivate::deactivate();
+        }
+
+        public function custom_post_type()
+        {
+            register_post_type('book', [ 'public' => true, 'label' => 'Books' ]);
+        }
+
+        public function enqueue()
+        {
+            wp_enqueue_style('myStyles', plugins_url('/assets/css/myStyles.css', __FILE__));
+            wp_enqueue_script('myScript', plugins_url('/assets/js/myScript.js', __FILE__));
+        }
     }
 
-    public function register()
-    {
-        add_action('admin_enqueue_scripts', [ $this, 'enqueue' ]);
-    }
-
-    public function activate()
-    {
-        $this->custom_post_type();
-        flush_rewrite_rules();
-    }
-
-    public function deactivate()
-    {
-        // flush_rewrite_rules();
-    }
-
-    public function uninstall()
-    {
-
-    }
-
-    public function custom_post_type()
-    {
-        register_post_type('book', [ 'public' => true, 'label' => 'Books' ]);
-    }
-
-    public function enqueue()
-    {
-        wp_enqueue_style('myStyles', plugins_url('/assets/css/myStyles.css', __FILE__));
-        wp_enqueue_script('myScript', plugins_url('/assets/js/myScript.js', __FILE__));
-    }
-}
-
-if (class_exists('MyPlugin')) {
     $myPlugin = new MyPlugin();
     $myPlugin->register();
-}
 
-register_activation_hook(__FILE__, [ $myPlugin, 'activate' ]);
-register_deactivation_hook(__FILE__, [ $myPlugin, 'deactivate' ]);
+    register_activation_hook(__FILE__, [ $myPlugin, 'activate' ]);
+    register_deactivation_hook(__FILE__, [ $myPlugin, 'deactivate' ]);
+
+}
